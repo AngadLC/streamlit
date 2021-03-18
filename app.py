d22ad9fd6ab3e0058c4e5c520b9349236ed5db23
Angad Lamichhane <71200732+AngadLC@users.noreply.github.com>
2021-03-18 07:15:17 +0545

    heruko deploy

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
+st.title('My first app')
 """
 # Streamlit and use of pandas to show in the web 
 
@@ -18,3 +20,25 @@ paris = air_quality['station_paris']
 if st.checkbox('Show data'):
     
     st.line_chart(paris)
+    #ploting using matplotlip
+    fig, ax = plt.subplots(figsize=(12, 4))
+    ax.plot(paris)
+    st.pyplot(fig)
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
